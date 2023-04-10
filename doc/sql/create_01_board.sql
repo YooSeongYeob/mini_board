c7a3c08045cc5e9ab56029b271d8fff212601bfc
YooSeongYeob <yoostar94@naver.com>
2023-04-10 11:46:46 +0900

    23.04.10 sql board_tbl up

---
+++ doc/sql/create_01_board.sql
@@ -0,0 +1,122 @@
+CREATE DATABASE board;
+
+USE board;
+
+CREATE TABLE board_info (
+	board_no INT PRIMARY KEY auto_increment
+	,board_title VARCHAR(100) NOT NULL 
+	,board_contents VARCHAR(1000) NOT NULL 
+	,board_wirte_date DATETIME NOT NULL
+	,board_del_flg CHAR(1) NOT NULL 
+	,board_del_date DATETIME
+);
+
+DESC board_info;
+
+
+ALTER TABLE board_info MODIFY board_del_flg CHAR(1) DEFAULT('0') NOT NULL;
+DELETE FROM table board_wirte_Date;
+
+ALTER TABLE board_info CHANGE board_wirte_date board_write_date DATETIME NOT NULL;
+
+
+INSERT INTO board_info (
+	 board_title
+	,board_contents
+	,board_write_date
+)
+VALUES(
+	'제목1'
+	,'내용1'
+	,NOW()
+)
+, (
+	'제목2'
+	,'내용2'
+	,NOW()
+)
+, (
+	'제목3'
+	,'내용3'
+	,NOW()
+)
+, (
+	'제목4'
+	,'내용4'
+	,NOW()
+)
+, (
+	'제목5'
+	,'내용5'
+	,NOW()
+)
+, (
+	'제목6'
+	,'내용6'
+	,NOW()
+)
+, (
+	'제목7'
+	,'내용7'
+	,NOW()
+)
+, (
+	'제목8'
+	,'내용8'
+	,NOW()
+), (
+	'제목9'
+	,'내용9'
+	,NOW()
+), (
+	'제목10'
+	,'내용10'
+	,NOW()
+), (
+	'제목11'
+	,'내용11'
+	,NOW()
+), (
+	'제목12'
+	,'내용12'
+	,NOW()
+), (
+	'제목13'
+	,'내용13'
+	,NOW()
+), (
+	'제목14'
+	,'내용14'
+	,NOW()
+), (
+	'제목15'
+	,'내용15'
+	,NOW()
+), (
+	'제목16'
+	,'내용16'
+	,NOW()
+), (
+	'제목17'
+	,'내용17'
+	,NOW()
+), (
+	'제목18'
+	,'내용18'
+	,NOW()
+)
+, (
+	'제목19'
+	,'내용19'
+	,NOW()
+), (
+	'제목20'
+	,'내용20'
+	,NOW()
+);
+
+SELECT * 
+FROM board_info;
+
+COMMIT;
+
